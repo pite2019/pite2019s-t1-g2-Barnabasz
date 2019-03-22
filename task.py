75d273c7a7235127f64984e733cea327b508eb64
Barnabasz <33337044+Barnabasz@users.noreply.github.com>
2019-03-22 14:28:16 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,28 +1,40 @@
-# Write a module that will simulate autonomic car.
-# The simulation is event based, an example:
-# car1 = Car()
-# car1.act(event)
-# print(car1.wheel_angle, car1.speed)
-# where event can be anything you want, i.e. :
-# `('obstacle', 10)` where `10` is a duration (time) of the event.
-##The program should:
-# - act on the event
-# - print out current steering wheel angle, and speed
-# - run in infinite loop
-# - until user breaks the loop
+class Car:
+	"""docstring for Car"""
+	def __init__(self, wheel_angle = 0, speed = 0):
+		self.wheel_angle = wheel_angle
+		self.speed = speed
+		self.engineStatus = False
+		self.seconds = 0
+	def loop(self):
+		while self.seconds < 200:
+			self.seconds += 1
+			self.speed -= 1
+			self.wheel_angle = -1
+			print("speed = {}".format(self.speed))
+			print("wheel_angle = {}".format(self.wheel_angle))
+	def startEngine(self, time):
+		if self.engineStatus == False:
+			print("WRRRR (on {}sec)".format(time))
+		else:
+			print("<engine is already running>")
+	def stopEngine(self, time):
+		if self.engineStatus == False:
+			print("<engine is not running>".format(time))
+		else:
+			print("UUUuum (on {}sec)".format(time))
+	def obsticleHandler(self, time):
+		pass
+	def act(self, event):
+		time = event[1]
+		event = event[0]
+		if event == 'stat the engine' :
+			self.startEngine(time)
+		elif event == 'stop the engine' :
+			self.stopEngine(time)
+		elif event == 'obsticle' :
+			self.obsticleHandler(time)
 
-#The level of realism in simulation is of your choice, but more sophisticated solutions are better.
-#If you can thing of any other features, you can add them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
-    
+
+car1 = Car()
+car1.act(('stat the engine', 10))
+car1.act(('stop the engine', 10))
